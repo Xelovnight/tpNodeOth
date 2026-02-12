5c4e2b3de9dbf79838f54d23fed76a4f5e89b102
Xelovnight <enzo.jungers@gmail.com>
2026-02-12 09:22:17 +0100

    tpmongooth2

--- index.js
+++ index.js
@@ -5,21 +5,33 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 const connectDb = require("./connectDb/connectDb");
 const path = require("path");
+const mongoose = require("mongoose");
 
 app.use(express.static(path.join(__dirname, "FrontEnd")));
-
-
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-});
-
-connectDb()
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "FrontEnd", "index.html"));
 });
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
 
 app.get("/othman", (req, res) => {
   res.send("Hello othman 👋");
 });
 
+app.get("/infos", async (req, res) => {
+    try {
+        const users = await mongoose.connection.db.collection('users').find().toArray();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+connectDb()
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
